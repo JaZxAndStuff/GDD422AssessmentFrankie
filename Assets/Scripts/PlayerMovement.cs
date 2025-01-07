5e05a26721f0149ab4e81acf8b1cca9a830b1ccd
UOC\2405082 <2405082@stu.chi.ac.uk>
2025-01-07 11:34:30 +0000

    starting player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2D;
+    Vector2 moveDir;
+   void OnMove(InputValue value)
+    {
+        moveDir = value.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        rb2D.velocity = moveDir * 5;
+    }
+}
